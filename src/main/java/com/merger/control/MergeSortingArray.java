52e5718d826e7af7914ce9bfbdd22204888632dd
Dregid <ufl852@gmail.com>
2023-02-06 21:35:36 +0600

    Реализован алгоритм сортировки слиянием строк + тест

--- src/main/java/com/merger/control/MergeSortingArray.java
+++ src/main/java/com/merger/control/MergeSortingArray.java
@@ -1,46 +1,90 @@
 package com.merger.control;
 
 public class MergeSortingArray {
-    public void mergeSort(int[] source, int size) {
+    public void mergeSortDigits(int[] source, int size) {
         if (size < 2) {
             return;
         }
         int mid = size / 2;
-        int[] leftHalf = new int[mid];
-        int[] rightHalf = new int[size - mid];
+        int[] lHalf = new int[mid];
+        int[] rHalf = new int[size - mid];
 
         for (int idx = 0; idx < mid; idx++) {
-            leftHalf[idx] = source[idx];
+            lHalf[idx] = source[idx];
         }
         for (int idx = mid; idx < size; idx++) {
-            rightHalf[idx - mid] = source[idx];
+            rHalf[idx - mid] = source[idx];
         }
-        mergeSort(leftHalf, mid);
-        mergeSort(rightHalf, size - mid);
+        mergeSortDigits(lHalf, mid);
+        mergeSortDigits(rHalf, size - mid);
 
-        merge(source, leftHalf, rightHalf,
+        mergeDigits(source, lHalf, rHalf,
                 mid, size - mid);
     }
 
-    public void merge(int[] result, int[] leftHalf, int[] rightHalf,
-                      int sizeLeft, int sizeRight) {
+    private void mergeDigits(int[] source, int[] leftHalf, int[] rightHalf,
+                             int lSize, int rSize) {
         int i = 0;
         int j = 0;
         int k = 0;
 
-        while (i < sizeLeft && j < sizeRight) {
+        while (i < lSize && j < rSize) {
             if (leftHalf[i] <= rightHalf[j]) {
-                result[k++] = leftHalf[i++];
+                source[k++] = leftHalf[i++];
             } else {
-                result[k++] = rightHalf[j++];
+                source[k++] = rightHalf[j++];
             }
         }
 
-        while (i < sizeLeft) {
-            result[k++] = leftHalf[i++];
+        while (i < lSize) {
+            source[k++] = leftHalf[i++];
         }
-        while (j < sizeRight) {
-            result[k++] = rightHalf[j++];
+        while (j < rSize) {
+            source[k++] = rightHalf[j++];
         }
+    }
+
+    public String[] mergeSortStrings(String[] source, int minSize, int maxSize) {
+        if(minSize == maxSize) {
+            return new String[]{source[minSize]};
+        }
+        int mid = minSize + (maxSize - minSize) / 2;
+        String[] lHalf = mergeSortStrings(source, minSize, mid);
+        String[] rHalf = mergeSortStrings(source, mid + 1, maxSize);
+
+        return mergeString(lHalf, rHalf);
+    }
+
+    private String[] mergeString(String[] lHalf, String[] rHalf) {
+        int lSize = lHalf.length;
+        int rSize = rHalf.length;
+        String[] result = new String[lSize + rSize];
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (j < lSize && k < rSize) {
+            if (isAlphabeticallySmaller(lHalf[j], rHalf[k])) {
+                result[i++] = lHalf[j++];
+            } else {
+                result[i++] = rHalf[k++];
+            }
+        }
+
+        while (j < lSize) {
+            result[i++] = lHalf[j++];
+        }
+        while (k < rSize) {
+            result[i++] = rHalf[k++];
+        }
+        return result;
+    }
+
+    private boolean isAlphabeticallySmaller(String str, String compared) {
+        str = str.toUpperCase();
+        compared = compared.toUpperCase();
+
+        return str.compareTo(compared) < 0;
     }
 }
